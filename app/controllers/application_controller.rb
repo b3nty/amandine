be8baa4fe5ae952e6b34b3823ec0df6aa352409f
b3nty <benty1988@hotmail.com>
2018-05-01 16:23:24 +0200

    sitemaps

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,14 @@ class ApplicationController < ActionController::Base
   self.responder = ApplicationResponder
   respond_to :html
 
+  def sitemap
+    path = Rails.root.join("public", "sitemaps", "sitemap.xml")
+    if File.exists?(path)
+      render xml: open(path).read
+    else
+      render text: "Sitemap not found.", status: :not_found
+    end
+  end
+
   protect_from_forgery with: :exception
 end
