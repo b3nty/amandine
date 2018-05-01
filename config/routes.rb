2d03a39c893e65db42162f4a7dea24f548007703
b3nty <benty1988@hotmail.com>
2018-05-01 16:29:04 +0200

    sitemaps

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,7 @@ Rails.application.routes.draw do
   match '/contact', :to => 'pages#contact', :via => [:get]
   match '/mentions-legales', :to => 'pages#mentions_legales', :via => [:get]
 
+  get "sitemap.xml" => "pages#sitemap", format: :xml, as: :sitemap
+
   root :to => 'pages#home'
 end
