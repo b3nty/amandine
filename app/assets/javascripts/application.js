1cb9aa18e190810f121f03929f14ebabfccfee74
b3nty <benty1988@hotmail.com>
2018-05-01 16:05:52 +0200

    google analytics

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,6 +20,15 @@
 //= require ladda
 //= require_tree .
 
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-118467617-1', 'auto');
+ga('send', 'pageview');
+setTimeout("ga('send','event','Temps passé sur la page','15 seconds')",15000);
+
 $( document ).on('turbolinks:load', function() {
   $('.carousel-home').carousel({
     interval: 3000,
