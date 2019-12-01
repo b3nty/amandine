c3207092274df7c61b6e3879947afb834cce523b
b3nty <benty1988@hotmail.com>
2019-12-02 00:20:30 +0100

    new website

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -31,21 +31,25 @@ setTimeout("ga('send','event','Temps passé sur la page','15 seconds')",15000);
 
 $( document ).on('turbolinks:load', function() {
 
-  $('.carousel[data-type="multi"] .item').each(function() {
-  	var next = $(this).next();
-  	if (!next.length) {
-  		next = $(this).siblings(':first');
-  	}
-  	next.children(':first-child').clone().appendTo($(this));
+  // Instantiate the Bootstrap carousel
+  $('.multi-item-carousel').carousel({
+    interval: false
+  });
 
-  	for (var i = 0; i < 2; i++) {
-  		next = next.next();
-  		if (!next.length) {
-  			next = $(this).siblings(':first');
-  		}
+  // for every slide in carousel, copy the next slide's item in the slide.
+  // Do the same for the next, next item.
+  $('.multi-item-carousel .item').each(function(){
+    var next = $(this).next();
+    if (!next.length) {
+      next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
 
-  		next.children(':first-child').clone().appendTo($(this));
-  	}
+    if (next.next().length>0) {
+      next.next().children(':first-child').clone().appendTo($(this));
+    } else {
+    	$(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+    }
   });
 
   var $animation_elements = $('.animation-element');
