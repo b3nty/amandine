5c08e2922b055c4564837374d5d9c88472e0faab
b3nty <benty1988@hotmail.com>
2020-05-04 23:33:25 +0200

    Lydia

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -31,6 +31,23 @@ setTimeout("ga('send','event','Temps passé sur la page','15 seconds')",15000);
 
 $( document ).on('turbolinks:load', function() {
 
+  /***************************   Facebook messenger   *******************************************/
+
+  window.fbAsyncInit = function() {
+    FB.init({
+      xfbml            : true,
+      version          : 'v6.0'
+    });
+  };
+
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = 'https://connect.facebook.net/fr_FR/sdk/xfbml.customerchat.js';
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+
   /***************************   centre   *******************************************/
   /* copy loaded thumbnails into carousel  */
   $('#gallery_centre .row .thumbnail').each(function(i) {
