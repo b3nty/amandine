fcc7769c02e7fc27ddf4975c9406f34fedb40dbd
b3nty <benty1988@hotmail.com>
2020-10-01 17:03:16 +0200

    oki

--- app/models/form_popup.rb
+++ app/models/form_popup.rb
@@ -1,5 +1,5 @@
 class FormPopup < ActiveRecord::Base
-  SPORT = [ "Rugby", "football", "triathlon", "tennis", "basket", "running", "handball",  "natation", "voile", "golf", "autre"]
-  NUMBER = [ "seul", "2", "3", "plus"]
+  SPORT = [ "rugby", "football", "triathlon", "tennis", "basket", "running", "handball",  "natation", "voile", "golf", "autre"]
+  NUMBER = [ "seul(e)", "2", "3", "plus"]
   CRENEAU = [ "mardi-jeudi 18h", "mercredi-vendredi 18h", "mercredi-vendredi 12h", "mardi-jeudi 12h", "mercredi-vendredi 17h"]
 end
